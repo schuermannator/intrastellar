778330bc013d766930ed0b4e45aebc9ec17d29ce
Zach Schuermann <zachary.zvs@gmail.com>
2019-04-16 00:47:35 -0500

    added lots for LCD

--- intrastellar/src/intrastellar.c
+++ intrastellar/src/intrastellar.c
@@ -13,6 +13,228 @@
 #endif
 
 #include <cr_section_macros.h>
+#include <stdint.h>
+
+//#define uint8_t unsigned char
+
+uint8_t lcd_read_data();
+
+// Colors (RGB565)
+#define	RA8875_BLACK            0x0000 ///< Black Color
+#define	RA8875_BLUE             0x001F ///< Blue Color
+#define	RA8875_RED              0xF800 ///< Red Color
+#define	RA8875_GREEN            0x07E0 ///< Green Color
+#define RA8875_CYAN             0x07FF ///< Cyan Color
+#define RA8875_MAGENTA          0xF81F ///< Magenta Color
+#define RA8875_YELLOW           0xFFE0 ///< Yellow Color
+#define RA8875_WHITE            0xFFFF ///< White Color
+
+// Command/Data pins for SPI
+#define RA8875_DATAWRITE        0x00
+#define RA8875_DATAREAD         0x40
+#define RA8875_CMDWRITE         0x80
+#define RA8875_CMDREAD          0xC0
+
+// Registers & bits
+#define RA8875_PWRR             0x01
+#define RA8875_PWRR_DISPON      0x80
+#define RA8875_PWRR_DISPOFF     0x00
+#define RA8875_PWRR_SLEEP       0x02
+#define RA8875_PWRR_NORMAL      0x00
+#define RA8875_PWRR_SOFTRESET   0x01
+
+#define RA8875_MRWC             0x02
+
+#define RA8875_GPIOX            0xC7
+
+#define RA8875_PLLC1            0x88
+#define RA8875_PLLC1_PLLDIV2    0x80
+#define RA8875_PLLC1_PLLDIV1    0x00
+
+#define RA8875_PLLC2            0x89
+#define RA8875_PLLC2_DIV1       0x00
+#define RA8875_PLLC2_DIV2       0x01
+#define RA8875_PLLC2_DIV4       0x02
+#define RA8875_PLLC2_DIV8       0x03
+#define RA8875_PLLC2_DIV16      0x04
+#define RA8875_PLLC2_DIV32      0x05
+#define RA8875_PLLC2_DIV64      0x06
+#define RA8875_PLLC2_DIV128     0x07
+
+#define RA8875_SYSR             0x10
+#define RA8875_SYSR_8BPP        0x00
+#define RA8875_SYSR_16BPP       0x0C
+#define RA8875_SYSR_MCU8        0x00
+#define RA8875_SYSR_MCU16       0x03
+
+#define RA8875_PCSR             0x04
+#define RA8875_PCSR_PDATR       0x00
+#define RA8875_PCSR_PDATL       0x80
+#define RA8875_PCSR_CLK         0x00
+#define RA8875_PCSR_2CLK        0x01
+#define RA8875_PCSR_4CLK        0x02
+#define RA8875_PCSR_8CLK        0x03
+
+#define RA8875_HDWR             0x14
+
+#define RA8875_HNDFTR           0x15
+#define RA8875_HNDFTR_DE_HIGH   0x00
+#define RA8875_HNDFTR_DE_LOW    0x80
+
+#define RA8875_HNDR             0x16
+#define RA8875_HSTR             0x17
+#define RA8875_HPWR             0x18
+#define RA8875_HPWR_LOW         0x00
+#define RA8875_HPWR_HIGH        0x80
+
+#define RA8875_VDHR0            0x19
+#define RA8875_VDHR1            0x1A
+#define RA8875_VNDR0            0x1B
+#define RA8875_VNDR1            0x1C
+#define RA8875_VSTR0            0x1D
+#define RA8875_VSTR1            0x1E
+#define RA8875_VPWR             0x1F
+#define RA8875_VPWR_LOW         0x00
+#define RA8875_VPWR_HIGH        0x80
+
+#define RA8875_HSAW0            0x30
+#define RA8875_HSAW1            0x31
+#define RA8875_VSAW0            0x32
+#define RA8875_VSAW1            0x33
+
+#define RA8875_HEAW0            0x34
+#define RA8875_HEAW1            0x35
+#define RA8875_VEAW0            0x36
+#define RA8875_VEAW1            0x37
+
+#define RA8875_MCLR             0x8E
+#define RA8875_MCLR_START       0x80
+#define RA8875_MCLR_STOP        0x00
+#define RA8875_MCLR_READSTATUS  0x80
+#define RA8875_MCLR_FULL        0x00
+#define RA8875_MCLR_ACTIVE      0x40
+
+#define RA8875_DCR                    0x90
+#define RA8875_DCR_LINESQUTRI_START   0x80
+#define RA8875_DCR_LINESQUTRI_STOP    0x00
+#define RA8875_DCR_LINESQUTRI_STATUS  0x80
+#define RA8875_DCR_CIRCLE_START       0x40
+#define RA8875_DCR_CIRCLE_STATUS      0x40
+#define RA8875_DCR_CIRCLE_STOP        0x00
+#define RA8875_DCR_FILL               0x20
+#define RA8875_DCR_NOFILL             0x00
+#define RA8875_DCR_DRAWLINE           0x00
+#define RA8875_DCR_DRAWTRIANGLE       0x01
+#define RA8875_DCR_DRAWSQUARE         0x10
+
+#define RA8875_ELLIPSE                0xA0
+#define RA8875_ELLIPSE_STATUS         0x80
+
+#define RA8875_MWCR0            0x40
+#define RA8875_MWCR0_GFXMODE    0x00
+#define RA8875_MWCR0_TXTMODE    0x80
+#define RA8875_MWCR0_CURSOR     0x40
+#define RA8875_MWCR0_BLINK      0x20
+
+#define RA8875_MWCR0_DIRMASK    0x0C ///< Bitmask for Write Direction
+#define RA8875_MWCR0_LRTD       0x00 ///< Left->Right then Top->Down
+#define RA8875_MWCR0_RLTD       0x04 ///< Right->Left then Top->Down
+#define RA8875_MWCR0_TDLR       0x08 ///< Top->Down then Left->Right
+#define RA8875_MWCR0_DTLR       0x0C ///< Down->Top then Left->Right
+
+#define RA8875_BTCR             0x44
+#define RA8875_CURH0            0x46
+#define RA8875_CURH1            0x47
+#define RA8875_CURV0            0x48
+#define RA8875_CURV1            0x49
+
+#define RA8875_P1CR             0x8A
+#define RA8875_P1CR_ENABLE      0x80
+#define RA8875_P1CR_DISABLE     0x00
+#define RA8875_P1CR_CLKOUT      0x10
+#define RA8875_P1CR_PWMOUT      0x00
+
+#define RA8875_P1DCR            0x8B
+
+#define RA8875_P2CR             0x8C
+#define RA8875_P2CR_ENABLE      0x80
+#define RA8875_P2CR_DISABLE     0x00
+#define RA8875_P2CR_CLKOUT      0x10
+#define RA8875_P2CR_PWMOUT      0x00
+
+#define RA8875_P2DCR            0x8D
+
+#define RA8875_PWM_CLK_DIV1     0x00
+#define RA8875_PWM_CLK_DIV2     0x01
+#define RA8875_PWM_CLK_DIV4     0x02
+#define RA8875_PWM_CLK_DIV8     0x03
+#define RA8875_PWM_CLK_DIV16    0x04
+#define RA8875_PWM_CLK_DIV32    0x05
+#define RA8875_PWM_CLK_DIV64    0x06
+#define RA8875_PWM_CLK_DIV128   0x07
+#define RA8875_PWM_CLK_DIV256   0x08
+#define RA8875_PWM_CLK_DIV512   0x09
+#define RA8875_PWM_CLK_DIV1024  0x0A
+#define RA8875_PWM_CLK_DIV2048  0x0B
+#define RA8875_PWM_CLK_DIV4096  0x0C
+#define RA8875_PWM_CLK_DIV8192  0x0D
+#define RA8875_PWM_CLK_DIV16384 0x0E
+#define RA8875_PWM_CLK_DIV32768 0x0F
+
+#define RA8875_TPCR0                  0x70
+#define RA8875_TPCR0_ENABLE           0x80
+#define RA8875_TPCR0_DISABLE          0x00
+#define RA8875_TPCR0_WAIT_512CLK      0x00
+#define RA8875_TPCR0_WAIT_1024CLK     0x10
+#define RA8875_TPCR0_WAIT_2048CLK     0x20
+#define RA8875_TPCR0_WAIT_4096CLK     0x30
+#define RA8875_TPCR0_WAIT_8192CLK     0x40
+#define RA8875_TPCR0_WAIT_16384CLK    0x50
+#define RA8875_TPCR0_WAIT_32768CLK    0x60
+#define RA8875_TPCR0_WAIT_65536CLK    0x70
+#define RA8875_TPCR0_WAKEENABLE       0x08
+#define RA8875_TPCR0_WAKEDISABLE      0x00
+#define RA8875_TPCR0_ADCCLK_DIV1      0x00
+#define RA8875_TPCR0_ADCCLK_DIV2      0x01
+#define RA8875_TPCR0_ADCCLK_DIV4      0x02
+#define RA8875_TPCR0_ADCCLK_DIV8      0x03
+#define RA8875_TPCR0_ADCCLK_DIV16     0x04
+#define RA8875_TPCR0_ADCCLK_DIV32     0x05
+#define RA8875_TPCR0_ADCCLK_DIV64     0x06
+#define RA8875_TPCR0_ADCCLK_DIV128    0x07
+
+#define RA8875_TPCR1            0x71
+#define RA8875_TPCR1_AUTO       0x00
+#define RA8875_TPCR1_MANUAL     0x40
+#define RA8875_TPCR1_VREFINT    0x00
+#define RA8875_TPCR1_VREFEXT    0x20
+#define RA8875_TPCR1_DEBOUNCE   0x04
+#define RA8875_TPCR1_NODEBOUNCE 0x00
+#define RA8875_TPCR1_IDLE       0x00
+#define RA8875_TPCR1_WAIT       0x01
+#define RA8875_TPCR1_LATCHX     0x02
+#define RA8875_TPCR1_LATCHY     0x03
+
+#define RA8875_TPXH             0x72
+#define RA8875_TPYH             0x73
+#define RA8875_TPXYL            0x74
+
+#define RA8875_INTC1            0xF0
+#define RA8875_INTC1_KEY        0x10
+#define RA8875_INTC1_DMA        0x08
+#define RA8875_INTC1_TP         0x04
+#define RA8875_INTC1_BTE        0x02
+
+#define RA8875_INTC2            0xF1
+#define RA8875_INTC2_KEY        0x10
+#define RA8875_INTC2_DMA        0x08
+#define RA8875_INTC2_TP         0x04
+#define RA8875_INTC2_BTE        0x02
+
+#define RA8875_SCROLL_BOTH      0x00
+#define RA8875_SCROLL_LAYER1    0x40
+#define RA8875_SCROLL_LAYER2    0x80
+#define RA8875_SCROLL_BUFFER    0xC0
 
 // GPIO
 #define FIO0DIR (*(volatile unsigned int *) 0x2009c000)
@@ -48,6 +270,12 @@
 // Systick
 #define STCTRL		(*(volatile unsigned int *) 0xE000E010)
 #define STRELOAD	(*(volatile unsigned int *) 0xE000E014)
+
+void wait_ticks(int count) {
+    volatile int ticks = count;
+    while (ticks > 0)
+        ticks--;
+}
 
 /*
  * setup CPU clock for 96 MHz instead of default 4MHz
@@ -93,16 +321,21 @@ void gpio_setup() {
 	FIO3DIR |= (3<<25);
 
 	// SPI SS
-	FIO0DIR |= (1<<9);
-	// port 8?
-
+	FIO0DIR |= (1<<9); // P0.9 is SS for PS2
+	FIO0DIR |= (1<<7); // P0.7 is SS for LCD
 }
 
 void spi_setup() {
+	PINSEL0 |= (3 << 30); // SCK
+	PINSEL1 |= (3 << 2); // MISO
+	PINSEL1 |= (3 << 4); // MOSI
+}
+
+void ps2_spi_setup() {
 	// 96 MHz / 8 => 12 MHz SPI clock
 	PCLKSEL0 |= (3<<16);
 
-	// 12 MHz / 48 MHz => 250kHz SCK
+	// 12 MHz / 48 => 250kHz SCK
 	S0SPCCR |= 0x30;
 
 	// phase
@@ -116,10 +349,29 @@ void spi_setup() {
 
 	// LSB first
 	S0SPCR |= (1<<6);
+}
 
-	PINSEL0 |= (3 << 30); // SCK
-	PINSEL1 |= (3 << 2); // MISO
-	PINSEL1 |= (3 << 4); // MOSI
+void lcd_spi_setup() {
+	// 96 MHz / 8 => 12 MHz SPI clock (11)
+	// 96 / 2 => 48 MHz (10)
+	PCLKSEL0 &= ~(1<<16);
+	PCLKSEL0 |= (1<<17);
+
+	// 48 MHz / 12 => 4 MHz SCK
+	// has to be even, >= 8
+	S0SPCCR |= 0xC;
+
+	// phase (default)
+	S0SPCR &= ~(1<<3);
+
+	// active low clock
+	S0SPCR |= (1<<4);
+
+	// operate in master mode
+	S0SPCR |= (1<<5);
+
+	// MSB first (default)
+	S0SPCR &= ~(1<<6);
 }
 
 void interrupt_setup() {
@@ -252,6 +504,237 @@ int read_controller() {
 	return controller;
 }
 
+void lcd_select() {
+	FIO0PIN &= ~(1<<7);
+}
+
+void lcd_free() {
+	FIO0PIN |= (1<<7);
+}
+
+void spi_begin() {
+	lcd_spi_setup();
+}
+
+uint8_t spi_send(uint8_t data) {
+	S0SPDR = data;
+	while((S0SPSR & (1<<7)) == 0) {}
+	int status = S0SPSR;
+	int read = S0SPDR;
+}
+
+void spi_end() {
+
+}
+
+/**************************************************************************/
+/*!
+    Write data to the specified register
+
+    @param reg Register to write to
+    @param val Value to write
+*/
+/**************************************************************************/
+void lcd_write_reg(uint8_t reg, uint8_t val) {
+	lcd_write_command(reg);
+	lcd_write_data(val);
+}
+
+/**************************************************************************/
+/*
+    Set the register to read from
+
+    @param reg Register to read
+
+    @return The value
+*/
+/**************************************************************************/
+uint8_t lcd_read_reg(uint8_t reg) {
+	lcd_write_command(reg);
+	return lcd_read_data();
+}
+
+/**************************************************************************/
+/*!
+    Write data to the current register
+
+    @param d Data to write
+*/
+/**************************************************************************/
+void lcd_write_data(uint8_t d)
+{
+	lcd_select();
+	spi_begin();
+	spi_send(RA8875_DATAWRITE);
+	spi_send(d);
+	spi_end();
+	lcd_free();
+}
+
+/**************************************************************************/
+/*!
+    Read the data from the current register
+
+    @return The Value
+*/
+/**************************************************************************/
+uint8_t lcd_read_data(void)
+{
+	lcd_select();
+	spi_begin();
+	spi_send(RA8875_DATAREAD);
+	uint8_t x = spi_send(0x0);
+	spi_end();
+	lcd_free();
+	return x;
+}
+
+/**************************************************************************/
+/*!
+    Write a command to the current register
+
+    @param d The data to write as a command
+ */
+/**************************************************************************/
+void lcd_write_command(uint8_t d)
+{
+	lcd_select();
+	spi_begin();
+	spi_send(RA8875_CMDWRITE);
+	spi_send(d);
+	spi_end();
+	lcd_free();
+}
+
+/**************************************************************************/
+/*!
+    Read the status from the current register
+    @return The value
+ */
+/**************************************************************************/
+uint8_t lcd_read_status(void)
+{
+	lcd_select();
+	spi_begin();
+	spi_send(RA8875_CMDREAD);
+	uint8_t x = spi_send(0x0);
+	spi_end();
+	lcd_free();
+	return x;
+}
+
+void lcd_pll_init() {
+	lcd_write_reg(RA8875_PLLC1, RA8875_PLLC1_PLLDIV1 + 10);
+	wait_ticks(100); //delay(1);
+	lcd_write_reg(RA8875_PLLC2, RA8875_PLLC2_DIV4);
+	wait_ticks(100); //delay(1);
+}
+
+// initialize
+void lcd_init(void) {
+	uint16_t _height = 480;
+	uint16_t _width = 800;
+
+	lcd_pll_init();
+	lcd_write_reg(RA8875_SYSR, RA8875_SYSR_16BPP | RA8875_SYSR_MCU8);
+
+	/* Timing values */
+	uint8_t pixclk;
+	uint8_t hsync_start;
+	uint8_t hsync_pw;
+	uint8_t hsync_finetune;
+	uint8_t hsync_nondisp;
+	uint8_t vsync_pw;
+	uint16_t vsync_nondisp;
+	uint16_t vsync_start;
+
+	pixclk          = RA8875_PCSR_PDATL | RA8875_PCSR_2CLK;
+	hsync_nondisp   = 26;
+	hsync_start     = 32;
+	hsync_pw        = 96;
+	hsync_finetune  = 0;
+	vsync_nondisp   = 32;
+	vsync_start     = 23;
+	vsync_pw        = 2;
+
+	lcd_write_reg(RA8875_PCSR, pixclk);
+	wait_ticks(100); //delay(1)
+
+	/* Horizontal settings registers */
+	lcd_write_reg(RA8875_HDWR, (_width / 8) - 1);                          // H width: (HDWR + 1) * 8 = 480
+	lcd_write_reg(RA8875_HNDFTR, RA8875_HNDFTR_DE_HIGH + hsync_finetune);
+	lcd_write_reg(RA8875_HNDR, (hsync_nondisp - hsync_finetune - 2)/8);    // H non-display: HNDR * 8 + HNDFTR + 2 = 10
+	lcd_write_reg(RA8875_HSTR, hsync_start/8 - 1);                         // Hsync start: (HSTR + 1)*8
+	lcd_write_reg(RA8875_HPWR, RA8875_HPWR_LOW + (hsync_pw/8 - 1));        // HSync pulse width = (HPWR+1) * 8
+
+	/* Vertical settings registers */
+	lcd_write_reg(RA8875_VDHR0, (uint16_t)(_height - 1) & 0xFF);
+	lcd_write_reg(RA8875_VDHR1, (uint16_t)(_height - 1) >> 8);
+	lcd_write_reg(RA8875_VNDR0, vsync_nondisp-1);                          // V non-display period = VNDR + 1
+	lcd_write_reg(RA8875_VNDR1, vsync_nondisp >> 8);
+	lcd_write_reg(RA8875_VSTR0, vsync_start-1);                            // Vsync start position = VSTR + 1
+	lcd_write_reg(RA8875_VSTR1, vsync_start >> 8);
+	lcd_write_reg(RA8875_VPWR, RA8875_VPWR_LOW + vsync_pw - 1);            // Vsync pulse width = VPWR + 1
+
+	/* Set active window X */
+	lcd_write_reg(RA8875_HSAW0, 0);                                        // horizontal start point
+	lcd_write_reg(RA8875_HSAW1, 0);
+	lcd_write_reg(RA8875_HEAW0, (uint16_t)(_width - 1) & 0xFF);            // horizontal end point
+	lcd_write_reg(RA8875_HEAW1, (uint16_t)(_width - 1) >> 8);
+
+	/* Set active window Y */
+	lcd_write_reg(RA8875_VSAW0, 0);                                        // vertical start point
+	lcd_write_reg(RA8875_VSAW1, 0);
+	lcd_write_reg(RA8875_VEAW0, (uint16_t)(_height - 1) & 0xFF);           // horizontal end point
+	lcd_write_reg(RA8875_VEAW1, (uint16_t)(_height - 1) >> 8);
+
+	/* Clear the entire window */
+	lcd_write_reg(RA8875_MCLR, RA8875_MCLR_START | RA8875_MCLR_FULL);
+	wait_ticks(5000); // delay(500)
+}
+
+void lcd_text_mode()
+{
+	/* Set text mode */
+	lcd_write_command(RA8875_MWCR0);
+	uint8_t temp = lcd_read_data();
+	temp |= RA8875_MWCR0_TXTMODE; // Set bit 7
+	lcd_write_data(temp);
+
+	/* Select the internal (ROM) font */
+	lcd_write_command(0x21);
+	temp = lcd_read_data();
+	temp &= ~((1<<7) | (1<<5)); // Clear bits 7 and 5
+	lcd_write_data(temp);
+	wait_ticks(1000);
+}
+
+void lcd_cursor_blink(uint8_t rate){
+	lcd_write_command(RA8875_MWCR0);
+	uint8_t temp = lcd_read_data();
+	temp |= RA8875_MWCR0_CURSOR;
+	lcd_write_data(temp);
+
+	lcd_write_command(RA8875_MWCR0);
+	temp = lcd_read_data();
+	temp |= RA8875_MWCR0_BLINK;
+	lcd_write_data(temp);
+
+	if (rate > 255) rate = 255;
+	lcd_write_command(RA8875_BTCR);
+	lcd_write_data(rate);
+	wait_ticks(1000);
+}
+
+int display() {
+	lcd_init();
+	lcd_text_mode();
+	lcd_cursor_blink(5);
+
+	return 0;
+}
+
+
 /*
  * Starts I2C communication
  * @param: void
@@ -261,7 +744,7 @@ void i2c_start() {
 	I2C1CONSET = (1<<3); // ****
 	I2C1CONSET = (1<<5); // SR style flip flop (this is readable)
 	I2C1CONCLR = (1<<3); // cannot read from here -- no OR's etc
-	while(((I2C1CONSET >> 3) & 1) == 0) {}
+	while(((I2C1CONSET >> 3) & 1) == 0) {} /////////STUCK HERE
 	I2C1CONCLR = (1<<5); // write to clear register
 }
 
@@ -362,11 +845,13 @@ int main(void) {
 	gpio_setup();
 	spi_setup();
 	//interrupt_setup();
-	eep_setup();
+	//eep_setup();
 	led(-1);
 
-	eep_write();
+	//eep_write();
 	//int test = eep_read();
+
+	display();
 
 	while (1) {}
     return 0;
